79d8c6d14b21f6f7419337e9da5d8bfbcf9096a7
Sebastian Romero <jusromerov@correo.udistrital.edu.co>
2022-12-07 13:33:51 -0500

    Se crearon dos componentes mas de canciones, en total se tienen tres componentes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -12,6 +12,20 @@ function App() {
           descripcion='Esta cancion es de las GOOD'
           imagen='https://i.ytimg.com/vi/W2MpGCL8-9o/sddefault.jpg?sqp=-oaymwEWCJADEOEBIAQqCghqEJQEGHgg6AJIWg&rs=AMzJL3kGUdhrVUVPeiSPM_CbYkWZv_ytYw'
         />
+
+        <Favoritos
+          nombre='Señorita'
+          web='https://www.youtube.com/watch?v=dFp_b5DPIIo'
+          descripcion='Un clasico de pandemia'
+          imagen='https://i.ytimg.com/vi/dFp_b5DPIIo/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCFZIfOVCl1mA7yPSfOf1AQA9x2iA'
+        />
+
+        <Favoritos
+          nombre="There's Nothing Holdin' Me Back"
+          web='https://www.youtube.com/watch?v=dT2owtxkU8k'
+          descripcion='Para los chicos malos'
+          imagen='https://i.ytimg.com/vi/dT2owtxkU8k/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD9T_GnkUEJtAlXDJfGae7bPM5GMQ'
+        />
       </div>
     </div>
   );

0c307f4da60fe01ed07713457ee6e9851e416a69
Michael-Steven-Jimenez-contreras <msjimenezc@correo.udistrital.edu.co>
2022-12-06 22:48:22 -0500

    Ahora puede mostrar una descripcion

--- scripts/favoritos.js
+++ scripts/favoritos.js
@@ -3,13 +3,25 @@ const root = ReactDOM.createRoot(
     document.getElementById('favs')
 );
 
+function desc(titulo, desc){
+    let c=e("div",{className:"favdesc"},[
+        e('h4',{},titulo),
+        e('br',{},null),
+        e('h5',{},desc)
+    ]);
+    return c;
+}
+
 function boton(imagen, titulo, descripcion){
-    let b=e('div',{className:"favorito"},
-        [e('img',{'src':imagen},null),
-        e('h4',{},titulo)]
-    );
+    let b=e('div',{className:"favorito"},[
+        e('img',{'src':imagen},null),
+        e('h4',{},titulo),
+        desc(titulo,descripcion)
+    ]);
     return b;
 }
+
+
 function cuad(botones){
     let li=[];
     for(let i of botones){
@@ -19,6 +31,8 @@ function cuad(botones){
     return ul;
 }
 
+
+/*inicializacion de la página*/
 let rutas=["../multimedia/f1.jpg","../multimedia/1.jpg","../multimedia/f1.jpg","../multimedia/1.jpg","../multimedia/f1.jpg"]
 let titulos=['Meme1345678765434567765432345676543','Meme2','Meme3','Meme4','Meme5']
 let b=[]
